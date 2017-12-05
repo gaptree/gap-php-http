b5abf830a3b23096bcd1826f2c85798175607560
zhanjh <zhanjh@126.com>
2017-12-05 14:44:06 +0800

    Add JsonResponse and RedirectResponse

---
+++ src/JsonResponse.php
@@ -0,0 +1,8 @@
+<?php
+namespace Gap\Http;
+
+use Symfony\Component\HttpFoundation\JsonResponse as SymfonyJsonResponse;
+
+class JsonResponse extends SymfonyJsonResponse
+{
+}
---
+++ src/RedirectResponse.php
@@ -0,0 +1,8 @@
+<?php
+namespace Gap\Http;
+
+use Symfony\Component\HttpFoundation\RedirectResponse as SymfonyRedirectResponse;
+
+class RedirectResponse extends SymfonyRedirectResponse
+{
+}
